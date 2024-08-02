f46bb505ffef3830dd2603eeb0b20ddddd5d0d0b
“Sara.z” <sara_zaabi_93.com>
2024-08-02 17:27:41 +0400

    new folder

---
+++ Sara/course practice/website/actions.js
@@ -0,0 +1,8 @@
+//console.log('Today')
+//console.log('we will create an alert');
+
+//window.alert('This is an alert!')
+
+document.getElementById("myId").textContent = 'Hello';
+document.getElementById("myP").textContent = 'Today is Thursday'
+
